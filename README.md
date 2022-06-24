bec49dc447498f309b6a2ea87e68464b26f04646
carmhack <carmhack@gmail.com>
2022-06-24 11:22:02 +0200

    add json-server and refactor challenge

--- README.md
+++ README.md
@@ -1,22 +1,24 @@
 # Debugging Books App
 
-Ciao collega, ho bisogno di te! Sono disperato! Devo consegnare un progetto ma il codice ha tanti bug e non riesco a risolverli.
-Puoi provare a sistemarli prima che arrivi il BOSS?
+Ciao collega, ho bisogno di te, sono disperato! Devo consegnare un progetto ma ho mille bug e non riesco a risolverli. Puoi provare a sistemarli prima che arrivi il BOSS?
 
-### Bug 1: Icons
+## General
+Avevo fretta e ho scritto tutto di getto. Sentiti libero di fare un po' di refactoring.
+
+### Get Books
+I libri non vengono visualizzati in pagina. All'avvio ho un alert di errore.
+
+### Reading Time
+Il tempo di lettura dei libri da tutti quei numeri decimali. Forse è meglio arrotondare.
+
+### Rating
+Il numero di stelle mostrate in base al rating è fuori scala. Dovrebbero essere 5 in totale, ma in alcuni casi ce ne sono 6.
+
+### Covers
+Di alcuni libri non viene mostrata l'immagine, eppure nella cartella c'è.
+
+### Icons
 Nella card del libro in alto a destra sono rappresentate delle icone che rappresentano il genere principale. Per alcuni libri però le icone non appaiono.
 
-### Bug 2: Rating
-Il numero di stelline non si trova. Dovrebbe essere in una scala da 1 a 5.
-
-### Bug 3: Reading Time
-Il tempo di lettura, in alcuni casi, da tanti numeri decimali. Magari potremmo metterne 2 al massimo.
-
-### Bug 4: Images
-Di alcuni libri non abbiamo le immagini nella cartella. Riesci a risolvere in qualche modo? In altri casi invece l'immagine c'è ma non riesce a prenderla correttamente.
-
-### Bug 5: Tags
-Vedi tutti quegli `undefined`? Quelli dovrebbero essere i tags del libro. Qualcosa non va.
-
-### Last but not least
-Avevo fretta e ho scritto tutto di getto. Se riesci a dare una sistemata con un po' di refactoring te ne sarei grato.
+### Tags
+Vedi tutti quegli `undefined` nelle card? Lì dovrebbero esserci i tags del libro, uno per ogni genere, con le label in italiano.
--- js/app.js
+++ js/app.js
@@ -18,170 +18,62 @@ const genresLabels = Object.freeze({
     biography: 'Autobiografia',
 });
 
-const books = [
-    {
-        id: 1,
-        author: 'Stefano Benni',
-        title: 'Il bar sotto il mare',
-        firstPublishingYear: '1987',
-        genres: ['stories'],
-        originalLanguage: 'Italiano',
-        pages: '180',
-        rating: '8',
-    },
-    {
-        id: 2,
-        author: 'Stefano Benni',
-        title: 'Stranalandia',
-        firstPublishingYear: '1984',
-        genres: ['novel'],
-        originalLanguage: 'Italiano',
-        pages: '150',
-        rating: '7.5',
-    },
-    {
-        id: 3,
-        author: 'Haruki Murakami',
-        title: 'Kafka sulla spiaggia',
-        firstPublishingYear: '2002',
-        genres: ['novel', 'fantasy'],
-        originalLanguage: 'Giapponese',
-        pages: '566',
-        rating: null,
-    },
-    {
-        id: 4,
-        author: 'Ernest Hemingway',
-        title: 'Il vecchio e il mare',
-        firstPublishingYear: '1952',
-        genres: ['novel', 'allegoric', 'sea'],
-        originalLanguage: 'Inglese',
-        pages: '175',
-        rating: '9.5',
-    },
-    {
-        id: 5,
-        author: 'Robert Louis Stevenson',
-        title: "L'isola del tesoro",
-        firstPublishingYear: '1883',
-        genres: ['novel', 'adventure'],
-        originalLanguage: 'Inglese',
-        pages: '190',
-        rating: '7',
-    },
-    {
-        id: 6,
-        author: 'Herman Melville',
-        title: 'Moby Dick',
-        firstPublishingYear: '1851',
-        genres: ['novel', 'adventure'],
-        originalLanguage: 'Inglese',
-        pages: '422',
-        rating: '8.5',
-    },
-    {
-        id: 7,
-        author: 'Erri De Luca',
-        title: 'Il contrario di uno',
-        firstPublishingYear: '2003',
-        genres: ['novel'],
-        originalLanguage: 'Italiano',
-        pages: '120',
-        rating: '6',
-    },
-    {
-        id: 8,
-        author: 'Diego De Silva',
-        title: 'Sono contrario alle emozioni',
-        firstPublishingYear: '2011',
-        genres: ['novel'],
-        originalLanguage: 'Italiano',
-        pages: '177',
-        rating: '6.5',
-    },
-    {
-        id: 9,
-        author: 'Tiziano Terzani',
-        title: "Un'indovino mi disse",
-        firstPublishingYear: '1995',
-        genres: ['travels', 'biography'],
-        originalLanguage: 'Italiano',
-        pages: '430',
-        rating: '5',
-    },
-    {
-        id: 10,
-        author: 'Maccio Capatonda',
-        title: "Libro",
-        firstPublishingYear: '2020',
-        genres: ['biography'],
-        originalLanguage: 'Italiano',
-        pages: '222',
-        rating: '10',
-    },
-    {
-        id: 11,
-        author: 'David Foster Wallace',
-        title: "Una cosa divertente che non farò mai più",
-        firstPublishingYear: '1997',
-        genres: ['humour'],
-        originalLanguage: 'Italiano',
-        pages: '150',
-        rating: '6',
-    },
-    {
-        id: 12,
-        author: 'Stephen King',
-        title: "Misery",
-        firstPublishingYear: '1987',
-        genres: ['thriller'],
-        originalLanguage: 'Inglese',
-        pages: '358',
-        rating: '8',
-    },
-]
+window.addEventListener('load', function() {
+    fetch('http://localhost:3000/books')
+    .then(function (response) {
+        response.json();
+    })
+    .then(function (result) {
+        displayBooks(result);
+    })
+    .catch(function (error) {
+        alert(error);
+    })
+});
 
-const booksContainer = document.querySelector('.books');
+function displayBooks(books) {
+    const booksContainer = document.querySelector('.books');
 
-books.forEach((book) => {
-    const tags = book.genres.forEach((genre) => {
-        return `<div class="book-tag">${genre}</div>`;
-    });
-
-    const chars = { ',': '', "'": '', '.': '', ' ': '', '"': '' };
-
-    let ratingString = '';
-    const rating = book.rating / 2;
-    for (let i = 1; i <= rating; i++) {
-        ratingString += '<img src="icons/star.png">';
-    }
-    if (rating - parseInt(rating) > 0.5) {
-        ratingString += '<img src="icons/half-star.png">';
-    }
-    for (let i = rating; i <= 5; i++) {
-        ratingString += '<img src="icons/star-empty.png">';
-    }
+    books.forEach((book) => {
+        const tags = book.genres.forEach((genre) => {
+            return `<div class="book-tag">${genre}</div>`;
+        });
     
-    booksContainer.innerHTML += `
-        <div class="book">
-            <div class="book-icons">
-                <img class="book-icon" src="icons/${iconsByGenres[book.genres[0]]}.png">
+        const chars = { ',': '', '.': '', ' ': '', '"': '' };
+    
+        let ratingString = '';
+        const rating = book.rating / 2;
+        for (let i = 1; i <= rating; i++) {
+            ratingString += '<img src="icons/star.png">';
+        }
+        if (rating - parseInt(rating) > 0.5) {
+            ratingString += '<img src="icons/half-star.png">';
+        }
+        for (let i = rating; i <= 5; i++) {
+            ratingString += '<img src="icons/star-empty.png">';
+        }
+        
+        booksContainer.innerHTML += `
+            <div class="book">
+                <div class="book-icons">
+                    <img class="book-icon" src="icons/${iconsByGenres[book.genres[0]]}.png">
+                </div>
+                <div class="book-image">
+                    <img src="images/${book.title.toLowerCase().replace(/[,.' "]/g, match => chars[match])}.jpeg" alt="${book.title}">
+                </div>
+                <h2 class="book-title">
+                    ${book.title}
+                    <span class="book-year">${book.firstPublishingYear}</span>
+                </h2>
+                <h3 class="book-author">di ${book.author}</h3>
+                <div class="book-tags">
+                    ${tags}
+                </div>
+                <div class="book-rating">
+                    ${ratingString}
+                </div>
+                <p class="book-reading-time">Tempo di lettura: ${book.pages / 30}</p>
             </div>
-            <div class="book-image">
-                <img src="images/${book.title.toLowerCase().replace(/[,.' "]/g, match => chars[match])}.jpeg" alt="${book.title}">
-            </div>
-            <h2 class="book-title">
-                ${book.title}
-                <span class="book-year">${book.firstPublishingYear}</span>
-            </h2>
-            <h3 class="book-author">di ${book.author}</h3>
-            <div class="book-tags">
-                ${tags}
-            </div>
-            <div class="book-rating">
-                ${ratingString}
-            </div>
-            <p class="book-reading-time">Tempo di lettura: ${book.pages / 30}</p>
-        </div>
-    `;
-})
+        `;
+    })
+}
